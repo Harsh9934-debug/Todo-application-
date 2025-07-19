40668a4d808c6a8b5f92f0ddb7a40f25f8414e2d
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-07-19 14:54:02 +0000

    Added the comment in the zod section in the type.js  file

--- types.js
+++ types.js
@@ -1,3 +1,5 @@
+// this is mainly for the validation  and using the Zod and  to install the zod is the 
+// npm install zod 
 const zod = require("zod")
 const createTodo = zod.object({
     title: zod.string(),
