550906449cb267e68b75e72ee9c99addc8b851e5
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-07-23 21:12:58 +0000

    Updated some of the changes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express  = require("express");
 const { createTodo } = require("./types");
 const { Todo } = require("./db");
+const cors = require("cors")
 const { todo } = require("node:test");
 const { title } = require("process");
 const app = express(); 
+app.use(cors());
+
 
 app.use(express.json());
 
@@ -54,6 +57,24 @@ app.get("/completed",async function(req,res){
         msg:"Todo mark has been updated "
     })
 })
+
+app.post("/completed", async function (req, res) {
+  const { id } = req.body;
+
+  if (!id) {
+    return res.status(400).json({ msg: "ID is required" });
+  }
+
+  try {
+    await Todo.updateOne({ _id: id }, { completed: true });
+
+    res.json({
+      msg: "Todo marked as completed"
+    });
+  } catch (err) {
+    res.status(500).json({ msg: "Something went wrong" });
+  }
+});
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
