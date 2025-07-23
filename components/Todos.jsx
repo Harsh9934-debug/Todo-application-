69e8c92a6cd0d72ec9406b52e9fd2c088c86fd3a
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-07-23 19:29:59 +0000

    updated some of the changes

--- components/Todos.jsx
+++ components/Todos.jsx
@@ -1,7 +1,7 @@
 export function Todos(){
     return <div>
         <h1>Go to gym </h1>
-        <h2>You need to go to the gym </h2>
+        <h5>You need to go to the gym </h5>
         <button>Mark as done </button>
         
     </div>
